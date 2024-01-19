29ec0915d594ec3cd4933e0225203f0f44e4c49b
Jackson Pruett <jacksonpruett@Jacksons-MBP.lan>
2024-01-19 08:18:05 -0500

    updated responsiveness of hero image and added first project

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,13 +1,14 @@
 import pro1 from '../assets/project1.jpg'
+import auto1 from '../assets/autoforge.png'
 
 const ProjectCardData = [
     {
-        imgsrc: pro1,
-        title: "Coming soon",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut ali",
-        view: "url.com",
-        source: "github.com",
-    }
+        imgsrc: auto1,
+        title: "AutoForge",
+        text: "AutoForge is a collaborative space that transforms individual automotive pursuits into a valuable resource, providing guidance and inspiration to fellow enthusiasts. CRUD App Built using React and Ruby on Rails.",
+        view: "https://auto-forge.onrender.com/",
+        source: "https://github.com/JavascriptJedis",
+    },
     // {
     //     imgsrc: pro1,
     //     title: "Coming soon",
