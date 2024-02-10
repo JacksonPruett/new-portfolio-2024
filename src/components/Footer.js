8c0289584d35c6e974dc35554170c6a3bc6ff12d
Jackson Pruett <jacksonpruett@Jacksons-MBP.lan>
2024-02-09 19:30:00 -0500

    add updated resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import "./Footerstyles.css";
-import resume from "../assets/Jackson_Pruett_resume.pdf"
+import resume from "../assets/JacksonPruett_resume-02_09_2024.pdf"
 import React from "react";
 
 import { FaFacebook, FaGithub, FaHome, FaLinkedin, FaMailBulk, FaPhone } from "react-icons/fa";
